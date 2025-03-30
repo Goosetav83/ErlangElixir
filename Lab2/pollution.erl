608256f3d5f1a99f7e148798273d175bd59f2c5d
Goosetav83 <gucwaajakub@gmail.com>
2025-03-30 18:35:14 +0200

    pollution finish

--- Lab2/pollution.erl
+++ Lab2/pollution.erl
@@ -10,9 +10,9 @@
 -author("maste").
 
 %% API
--export([create_monitor/0,add_station/3, add_value/5, remove_value/4]).
+-export([create_monitor/0,add_station/3, add_value/5, remove_value/4, get_one_value/4,get_station_min/3,get_daily_mean/3,get_area_mean/4]).
 %Stations -> [Name, {X,Y}]
-%Measurements -> [{Coords, Date, Type, Value}]
+%Measurements -> [{Coords, {Date, Time}, Type, Value}]
 -record(monitor, {stations = [], measurements = []}).
 
 create_monitor()->#monitor{}.
@@ -38,17 +38,17 @@ find_station(Identification,[H|Stations],I)->
     _ -> find_station(Identification, Stations, I+1)
   end.
 
-add_to_measurements(Coords,Date,Type,Value,Measurements)->
+add_to_measurements(Coords,DateTime,Type,Value,Measurements)->
   M = [X || X<- Measurements, case X of
-                                {Coords,Date,Type,_} -> true;
+                                {Coords,DateTime,Type,_} -> true;
                                 _ -> false
                               end],
   case M of
-    [] -> [{Coords,Date,Type,Value}|Measurements];
+    [] -> [{Coords,DateTime,Type,Value}|Measurements];
     _->{error, "Invalid measurement"}
   end.
 
-add_value(Identification, Date, Type, Value, #monitor{stations = Stations,measurements = Measurements} = Monitor)->
+add_value(Identification, DateTime, Type, Value, #monitor{stations = Stations,measurements = Measurements} = Monitor)->
   Stat = case find_station(Identification,Stations,1) of
            0 -> {error, "Station not found"};
            X -> lists:nth(X,Stations)
@@ -56,52 +56,68 @@ add_value(Identification, Date, Type, Value, #monitor{stations = Stations,measur
 
   case Stat of
     {error, _} -> Stat;
-    {_,Coords} -> case add_to_measurements(Coords,Date,Type,Value,Measurements) of
+    {_,Coords} -> case add_to_measurements(Coords,DateTime,Type,Value,Measurements) of
                     {error,_} -> {error, "Invalid measurement"};
                     M -> Monitor#monitor{measurements = M}
                   end
   end.
 
 
-remove_value(Identification, Date, Type, #monitor{stations = Stations, measurements = Measurements} = Monitor) ->
+remove_value(Identification, DateTime, Type, #monitor{stations = Stations, measurements = Measurements} = Monitor) ->
+  Stat = case find_station(Identification,Stations,1) of
+           0 -> {error, "Station not found"};
+           X -> lists:nth(X,Stations)
+         end,
+  NewMeasurement = case Stat of
+    {error,_} -> Stat;
+    {_,Coords} -> [X || X<-Measurements, case X of
+                                           {Coords,DateTime,Type,_} -> false;
+                                           _ -> true
+                                         end]
+  end,
+  case NewMeasurement of
+    {error,_} -> NewMeasurement;
+    Measurements -> {error, "Measurement not found"};
+    _ -> Monitor#monitor{measurements = NewMeasurement}
+end.
+
+get_one_value(Identification, DateTime, Type, #monitor{stations = Stations, measurements = Measurements}) ->
   Stat = case find_station(Identification,Stations,1) of
            0 -> {error, "Station not found"};
            X -> lists:nth(X,Stations)
          end,
   case Stat of
     {error,_} -> Stat;
-    {_,Coords} -> Monitor#monitor{measurements = [Z || Z<-Measurements, case Z of
-                                                                          {Coords, Date, Type, _} ->false;
-                                                                          _->true
-                                                                        end]}
+    {_,Coords} ->case [V || {C,DT,T,V}<-Measurements, C=:= Coords, DT =:= DateTime, T=:=Type] of
+                   [] -> {error, "Measurement not found"};
+                   [V|_] -> V
+                 end
   end.
 
-
-
-get_one_value(Identification, Date, Type, #monitor{stations = Stations, measurements = Measurements} = Monitor) ->
-  Stat = case find_station(Identification,Stations,1) of
-           0 -> {error, "Station not found"};
-           X -> lists:nth(X,Stations)
-         end,
-  case Stat of
-    {error,_} -> Stat;
-    {_,Coords} ->lists:search(
-                  fun
-                    ({C, D, T, V}) when C =:= Coords, D =:= Date, T =:= Type -> V;
-                    (_) -> {error, "Measurement not found"}
-                  end, Measurements)
-  end.
-
-get_station_min(Identification, Type, #monitor{stations = Stations, measurements = Measurements} = Monitor) ->
+get_station_min(Identification, Type, #monitor{stations = Stations, measurements = Measurements}) ->
   Stat = case find_station(Identification,Stations,1) of
            0 ->{error, "Station not found"};
            X -> lists:nth(X, Stations)
          end,
   case Stat of
     {error,_}-> Stat;
-    {_,Coords} ->lists:min([element(4,V) || V<-Measurements, element(1,V)=:=Coords, element(3,V) =:= Type])
+    {_,Coords} -> M = ([V || {C,_,T,V}<-Measurements,C =:= Coords ,T=:=Type]),
+                case M of
+                  [] -> {error, "No measurements"};
+                  _ -> lists:min(M)
+                end
   end.
 
-get_daily_mean(Date, Type, #monitor{stations = Stations, measurements = Measurements}=Monitor) ->
-  Values = [element(4,V)|| V<-Measurements, element(2,V) =:= Date, element(3,V) =:= Type],
-  lists:sum(Values)/length(Values).
+get_daily_mean(Type,Date, #monitor{measurements = Measurements}) ->
+  Values = [V || {_,{D,_},T,V} <- Measurements,D ==Date, T==Type],
+  case Values of
+    [] -> {error, "No measurements"};
+    _ -> lists:sum(Values)/length(Values)
+  end.
+
+get_area_mean(Type, {X,Y}, Radius, #monitor{measurements = Measurements}) ->
+  M = [V || {{X1,Y1}, _, T, V} <- Measurements, T == Type, math:sqrt((X1-X)*(X1-X) + (Y1-Y)*(Y1-Y)) =< Radius],
+  case M of
+    [] -> {error, "No measurements"};
+    _ -> lists:sum(M)/length(M)
+  end.
